811a00cdb511d752af850293459ec6b49ae11247
josuevfranco <76227878+josuevfranco@users.noreply.github.com>
2021-05-08 22:50:19 -0500

    Exa2 TW UAA

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  descrip : string = "Tecnologías Web - ISC UAA";
+
   constructor() { }
 
   ngOnInit(): void {
